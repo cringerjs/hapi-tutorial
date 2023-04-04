41de1e75c50978d31545a1dfaf9996496499fb98
Jeremy Schaffer <jeremys@codenexus.org>
2023-04-04 14:05:40 -0700

    Change to live/prod mode

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SK_TEST);
+const stripe = require('stripe')(process.env.STRIPE_SK_LIVE);
 // const endpointSecret = 'whsec_zEFwzuPP81dqYOvx15FN6uD0EgxcEsEY';
 
 const getEvent = (request, h) => {
